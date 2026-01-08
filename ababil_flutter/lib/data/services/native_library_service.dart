2a3c1ab9dc25f6644267ae76d0612d67b0e4220d
Samin yeasar Sohag <sssohag172@gmail.com>
2026-01-09 01:24:56 +0600

    improvements

--- ababil_flutter/lib/data/services/native_library_service.dart
+++ ababil_flutter/lib/data/services/native_library_service.dart
@@ -2,7 +2,7 @@ import 'dart:ffi';
 import 'dart:io';
 import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:path_provider/path_provider.dart';
 
 typedef FreeStringNative = Void Function(Pointer<Utf8>);
 typedef FreeStringDart = void Function(Pointer<Utf8>);
@@ -23,7 +23,8 @@ class NativeLibraryService {
       } else if (Platform.isIOS) {
         _dylib = DynamicLibrary.process();
       } else if (Platform.isMacOS) {
-        await _initializeMacOS();
+        _dylib = DynamicLibrary.open('libababil_core.dylib');
+        // await _initializeMacOS();
       } else if (Platform.isLinux) {
         _dylib = DynamicLibrary.open('libababil_core.so');
       } else if (Platform.isWindows) {
@@ -48,91 +49,91 @@ class NativeLibraryService {
     }
   }
 
-  static Future<void> _initializeMacOS() async {
-    // Get the executable path to find the app bundle
-    final executablePath = Platform.resolvedExecutable;
-    final executableDir = File(executablePath).parent.path;
+  // static Future<void> _initializeMacOS() async {
+  //   // Get the executable path to find the app bundle
+  //   final executablePath = Platform.resolvedExecutable;
+  //   final executableDir = File(executablePath).parent.path;
 
-    // List of paths to try
-    final List<String> pathsToTry = [];
+  //   // List of paths to try
+  //   final List<String> pathsToTry = [];
 
-    // 1. Try app bundle Frameworks (Contents/Frameworks inside .app)
-    pathsToTry.add('$executableDir/../Frameworks/libababil_core.dylib');
+  //   // 1. Try app bundle Frameworks (Contents/Frameworks inside .app)
+  //   pathsToTry.add('$executableDir/../Frameworks/libababil_core.dylib');
 
-    // 2. Try app bundle Resources (alternative location)
-    pathsToTry.add('$executableDir/../Resources/libababil_core.dylib');
+  //   // 2. Try app bundle Resources (alternative location)
+  //   pathsToTry.add('$executableDir/../Resources/libababil_core.dylib');
 
-    // 3. Try development path (when running with flutter run)
-    final currentDir = Directory.current.path;
-    pathsToTry.add('$currentDir/macos/Runner/Frameworks/libababil_core.dylib');
-    pathsToTry.add(
-      '$currentDir/ababil_flutter/macos/Runner/Frameworks/libababil_core.dylib',
-    );
+  //   // 3. Try development path (when running with flutter run)
+  //   final currentDir = Directory.current.path;
+  //   pathsToTry.add('$currentDir/macos/Runner/Frameworks/libababil_core.dylib');
+  //   pathsToTry.add(
+  //     '$currentDir/ababil_flutter/macos/Runner/Frameworks/libababil_core.dylib',
+  //   );
 
-    // 4. Try Documents directory approach
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      pathsToTry.add('${directory.path}/../Frameworks/libababil_core.dylib');
-    } catch (e) {
-      // Ignore if we can't get documents directory
-    }
+  //   // 4. Try Documents directory approach
+  //   try {
+  //     final directory = await getApplicationDocumentsDirectory();
+  //     pathsToTry.add('${directory.path}/../Frameworks/libababil_core.dylib');
+  //   } catch (e) {
+  //     // Ignore if we can't get documents directory
+  //   }
 
-    // 5. Try relative to executable
-    pathsToTry.add('libababil_core.dylib');
+  //   // 5. Try relative to executable
+  //   pathsToTry.add('libababil_core.dylib');
 
-    // 6. Try absolute path from project root
-    final homeDir = Platform.environment['HOME'] ?? '';
-    if (homeDir.isNotEmpty) {
-      pathsToTry.add(
-        '$homeDir/Projects/open-source/ababil/ababil_flutter/macos/Runner/Frameworks/libababil_core.dylib',
-      );
-      pathsToTry.add(
-        '$homeDir/Projects/open-source/ababil/ababil_core/target/release/libababil_core.dylib',
-      );
-    }
+  //   // 6. Try absolute path from project root
+  //   final homeDir = Platform.environment['HOME'] ?? '';
+  //   if (homeDir.isNotEmpty) {
+  //     pathsToTry.add(
+  //       '$homeDir/Projects/open-source/ababil/ababil_flutter/macos/Runner/Frameworks/libababil_core.dylib',
+  //     );
+  //     pathsToTry.add(
+  //       '$homeDir/Projects/open-source/ababil/ababil_core/target/release/libababil_core.dylib',
+  //     );
+  //   }
 
-    // 7. Try simple paths
-    pathsToTry.add(
-      '${Directory.current.path}/ababil_core/target/release/libababil_core.dylib',
-    );
-    pathsToTry.add(
-      '${Directory.current.path}/target/release/libababil_core.dylib',
-    );
-    pathsToTry.add('/usr/local/lib/libababil_core.dylib');
+  //   // 7. Try simple paths
+  //   pathsToTry.add(
+  //     '${Directory.current.path}/ababil_core/target/release/libababil_core.dylib',
+  //   );
+  //   pathsToTry.add(
+  //     '${Directory.current.path}/target/release/libababil_core.dylib',
+  //   );
+  //   pathsToTry.add('/usr/local/lib/libababil_core.dylib');
 
-    for (final path in pathsToTry) {
-      try {
-        _dylib = DynamicLibrary.open(path);
-        if (kDebugMode) {
-          print(
-            'NativeLibraryService: Successfully loaded library from: $path',
-          );
-        }
-        break;
-      } catch (e) {
-        if (kDebugMode) {
-          print('NativeLibraryService: Error loading library from: $path: $e');
-        }
-      }
-    }
+  //   for (final path in pathsToTry) {
+  //     try {
+  //       _dylib = DynamicLibrary.open(path);
+  //       if (kDebugMode) {
+  //         print(
+  //           'NativeLibraryService: Successfully loaded library from: $path',
+  //         );
+  //       }
+  //       break;
+  //     } catch (e) {
+  //       if (kDebugMode) {
+  //         print('NativeLibraryService: Error loading library from: $path: $e');
+  //       }
+  //     }
+  //   }
 
-    if (_dylib == null) {
-      if (kDebugMode) {
-        print(
-          'NativeLibraryService: Could not load libababil_core.dylib from any of these paths:',
-        );
-        print('Tried paths: $pathsToTry');
-        print('\nTo fix this:');
-        print(
-          '1. Build the Rust core: cd ababil_core && cargo build --release',
-        );
-        print(
-          '2. Copy the library: cp ababil_core/target/release/libababil_core.dylib ababil_flutter/macos/Runner/Frameworks/',
-        );
-        print('3. Or run: ./build.sh');
-      }
-    }
-  }
+  //   if (_dylib == null) {
+  //     if (kDebugMode) {
+  //       print(
+  //         'NativeLibraryService: Could not load libababil_core.dylib from any of these paths:',
+  //       );
+  //       print('Tried paths: $pathsToTry');
+  //       print('\nTo fix this:');
+  //       print(
+  //         '1. Build the Rust core: cd ababil_core && cargo build --release',
+  //       );
+  //       print(
+  //         '2. Copy the library: cp ababil_core/target/release/libababil_core.dylib ababil_flutter/macos/Runner/Frameworks/',
+  //       );
+  //       print('3. Or run: ./build.sh');
+  //     }
+  //   }
+  // }
 
   /// Get the loaded dynamic library
   static DynamicLibrary? get library => _dylib;
